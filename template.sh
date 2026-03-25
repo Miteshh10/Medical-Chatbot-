159371b3f75856cdd0a747c87a727e9214855dc0
Mitesh Yogesh Bhamre <miteshbhamre10@gmail.com>
2026-03-25 17:44:37 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#creating dir
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/traials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "dir and file" 
